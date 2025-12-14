6ea4ec90baf6fccc4b35290f36c8c1bd8ff65a54
OlimSadullaev <olimxonnutfullaevich@gmail.com>
2025-12-15 00:52:22 +0500

    Project Configuration

---
+++ TechnoPro/Services/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TechnoPro.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
