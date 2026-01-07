87ec25d9c9767e64c38b7633141a971d6143af3a
OlimSadullaev <olimxonnutfullaevich@gmail.com>
2026-01-07 21:59:48 +0500

    Order and OrderItem Created

---
+++ TechnoPro/Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TechnoPro.Models
+{
+	public class OrderItem
+	{
+		public int Id { get; set; }
+		public int Quantity { get; set; }
+
+		[Precision(16, 2)]
+		public decimal UnitPrice { get; set; }
+
+		// navigation property
+		public Product Product { get; set; } = new Product();
+	}
+}
--- TechnoPro/Services/ApplicationDbContext.cs
+++ TechnoPro/Services/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace TechnoPro.Services
         }
 
         public DbSet<Product> Products { get; set; }
-    }
+		public DbSet<Order> Orders { get; set; }
+	}
 }
