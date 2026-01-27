8885b1d558a18d8f7cbb0f8217aaf279cb792ffc
OlimSadullaev <olimxonnutfullaevich@gmail.com>
2026-01-27 23:16:44 +0500

    AdminOrders

---
+++ TechnoPro/Controllers/AdminOrdersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechnoPro.Services;
+
+namespace TechnoPro.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("Admin/Orders/{action=Index}/{id?}")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly int PageSize = 10;
+
+        public AdminOrdersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var orders = context.Orders.OrderByDescending(o => o.Id).ToList();
+
+            ViewBag.Orders = orders;
+
+            return View();
+        }
+    }
+}
